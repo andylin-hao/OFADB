f228b30a4648be322ef322d0f4ecb81c8dd90130
linlinhaohao888 <linlinhaohao.888@163.com>
2019-06-04 14:09:03 +0800

    - Add column values completion to insert expression.

--- src/main/java/expression/insert/InsertExpr.java
+++ src/main/java/expression/insert/InsertExpr.java
@@ -1,12 +1,17 @@
 package expression.insert;
 
 import expression.Expression;
+import meta.ColumnInfo;
+import meta.MetaData;
+import meta.TableInfo;
 import utils.Utils;
 import expression.select.RelationExpr;
 import types.ExprTypes;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
 
 public class InsertExpr extends Expression {
     private RelationExpr table = new RelationExpr();
@@ -39,9 +44,39 @@ public class InsertExpr extends Expression {
         return values;
     }
 
+    private void completeValues() throws IOException {
+        TableInfo tableInfo = MetaData.getTableInfoByName(table.getDbName(), table.getTableName());
+        ColumnInfo[] columnInfos = Objects.requireNonNull(tableInfo).columns;
+        HashMap<String, Integer> columnIndexMap = new HashMap<>();
+        for (int i = 0;i<columns.size();i++) {
+            columnIndexMap.put(columns.get(i), i);
+        }
+
+        columns.clear();
+        for (ColumnInfo columnInfo: columnInfos) {
+            columns.add(columnInfo.columnName);
+        }
+
+        ArrayList<ArrayList<Object>> newValues = new ArrayList<>();
+        for (ArrayList<Object> value: values) {
+            ArrayList<Object> newValue = new ArrayList<>();
+            for (ColumnInfo columnInfo: columnInfos) {
+                if (columnIndexMap.containsKey(columnInfo.columnName)) {
+                    newValue.add(value.get(columnIndexMap.get(columnInfo.columnName)));
+                } else {
+                    newValue.add(null);
+                }
+            }
+            newValues.add(newValue);
+        }
+
+        values.clear();
+        values = newValues;
+    }
+
     @Override
     public void checkValidity() throws IOException {
-        //TODO:补充完整values输出
         Utils.checkColumnsValues(table, columns, values);
+        completeValues();
     }
 }
